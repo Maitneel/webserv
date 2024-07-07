0cbf11fba50865ddcde9fc27ce556454c409e53f
taksaito <taksaito@student.42tokyo.jp>
2024-07-07 19:08:52 +0900

    wip

---
+++ srcs/config/config.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Locastion {
+    std::string name;
+    std::string root;
+    std::vector<std::string> methods;
+    // TODO: implement
+};
+
+class Server {
+    
+}
