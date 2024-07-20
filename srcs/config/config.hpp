59ef2673009d7fa98d993fea4d13b9ac484f0f7c
taksaito <taksaito@student.42tokyo.jp>
2024-07-20 20:25:39 +0900

    feat: configのclass定義を仮追加

--- srcs/config/config.hpp
+++ srcs/config/config.hpp
@@ -3,13 +3,27 @@
 #include <string>
 #include <vector>
 
-class Locastion {
-    std::string name;
-    std::string root;
-    std::vector<std::string> methods;
-    // TODO: implement
+enum HTTPMethod {
+    GET = 0,
+    POST,
+    DELETE,
 };
 
-class Server {
-    
-}
+class LocastionConfig {
+public:
+    std::string             name;
+    std::string             document_root;
+    std::vector<HTTPMethod> methods;
+    bool                    autoindex;
+    std::string             cgi_path;
+    size_t                  max_body_size;
+    std::string             redirect;
+};
+
+class ServerConfig {
+public:
+    std::vector<std::string> server_names;
+    std::string document_root;
+    int         port;
+    std::vector<LocastionConfig> location_configs;
+};
