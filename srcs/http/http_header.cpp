f0c2f46c6b9c43bbe41fcbc2e95f629b1e41c242
Maitneel <39688304+Maitneel@users.noreply.github.com>
2024-10-09 06:07:14 +0900

    fix: throw exception, if there is whitespace between field-name and colon

--- srcs/http/http_header.cpp
+++ srcs/http/http_header.cpp
@@ -13,10 +13,21 @@
 #include "extend_stdlib.hpp"
 
 std::pair<std::string, std::string> make_header_pair(std::string src) {
-    std::string filed_name = get_first_token(src);
-    std::string::size_type filed_value_start = filed_name.length() + 1;
-    to_lower(&filed_name);
-    std::string filed_value = src.substr(filed_value_start, src.length() - filed_value_start - 2);
+    std::string filed_name, filed_value;
+    try {
+        filed_name = get_first_token(src);
+        to_lower(&filed_name);
+        std::string::size_type filed_value_start = filed_name.length() + 1;
+        if (src.at(filed_name.length()) != ':') {
+            // TODO(someone): resopnse with 400 status code
+            // https://www.rfc-editor.org/rfc/rfc9112.html#section-5.1-2
+            throw HTTPRequest::InvalidRequest(kHTTPHeader);
+        }
+        filed_value = src.substr(filed_value_start, src.length() - filed_value_start - 2);
+    } catch (...) {
+        // TODO(someone): resopnse with 400 status code
+        throw HTTPRequest::InvalidRequest(kHTTPHeader);
+    }
     return std::make_pair(filed_name, filed_value);
 }
 
