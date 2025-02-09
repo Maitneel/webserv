878bd7cc0d33bd53f32a7e20be3f7c26bd4eef65
Maitneel <39688304+Maitneel@users.noreply.github.com>
2025-02-09 16:42:22 +0900

    fix: content-typeが適切に設定されない問題

--- srcs/server/server.cpp
+++ srcs/server/server.cpp
@@ -209,12 +209,11 @@ std::string GetContentType(const std::string path) {
 
     std::string ext = get_extension(path);
 
-    if (ext == "html") {
+    if (ext == ".html") {
         return "text/html";
-    } else if (ext == "txt") {
+    } else if (ext == ".txt") {
         return "text/plain";
     }
-    // TODO(taksaito): 他の MIME タイプの対応
     return "application/octet-stream";
 }
 
@@ -261,6 +260,7 @@ void Server::GetMethodHandler(HTTPContext *context, const std::string &req_path,
     int fd = open(path.c_str(), (O_RDONLY | O_NONBLOCK | O_CLOEXEC));
     if (0 <= fd) {
         context->content_type = GetContentType(path);
+        cerr << "content_type: " << context->content_type << endl;
         context->file_fd_ = fd;
         dispatcher_.RegisterFileFd(fd, connection_fd);
         return;
